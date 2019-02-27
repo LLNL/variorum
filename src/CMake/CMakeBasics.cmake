30887264a2594a42fe294f7703590ea5deaf34ed
Stephanie Brink <brink2@llnl.gov>
2019-02-26 17:08:30 -0800

    - Improve output statements for CMake configure - Specify v4.11.0 tag for breathe download as an external project. This works   with Sphinx 1.6.
    Signed-off-by: Stephanie Brink <brink2@llnl.gov>

--- src/CMake/CMakeBasics.cmake
+++ src/CMake/CMakeBasics.cmake
@@ -1,6 +1,6 @@
-################################
-# Standard CMake Options
-################################
+###########################
+# Standard CMake Options #
+###########################
 
 
 ## Fail if someone tries to config an in-source build.
@@ -15,24 +15,61 @@ set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 message(STATUS "CMake build tool name: ${CMAKE_BUILD_TOOL}")
 
+### Enable warnings
 macro(ENABLE_WARNINGS)
     add_definitions(-Wall -Wextra)
 endmacro()
 
+##########################
+# Platform Architectures #
+##########################
 if(VARIORUM_WITH_INTEL)
     message(STATUS "Building support for Intel architectures (VARIORUM_WITH_INTEL == ON)")
+else()
+    message(STATUS "Building without support for Intel architectures (VARIORUM_WITH_INTEL == OFF)")
 endif()
 if(VARIORUM_WITH_IBM)
     message(STATUS "Building support for IBM architectures (VARIORUM_WITH_IBM == ON)")
+else()
+    message(STATUS "Building without support for IBM architectures (VARIORUM_WITH_IBM == OFF)")
+endif()
+if(VARIORUM_WITH_AMD)
+    message(STATUS "Building support for AMD architectures (VARIORUM_WITH_AMD == ON)")
+else()
+    message(STATUS "Building without support for AMD architectures (VARIORUM_WITH_AMD == OFF)")
+endif()
+if(VARIORUM_WITH_GPU)
+    message(STATUS "Building support for GPU architectures (VARIORUM_WITH_GPU == ON)")
+else()
+    message(STATUS "Building without support for GPU architectures (VARIORUM_WITH_GPU == OFF)")
 endif()
 
+#############
+# DEBUGGING #
+#############
+if (VARIORUM_DEBUG)
+    message(STATUS "Building with debug statements (VARIORUM_DEBUG = ON)")
+else()
+    message(STATUS "Building without debug statements (VARIORUM_DEBUG = OFF)")
+endif()
+if (VARIORUM_LOG)
+    message(STATUS "Building with logging statements (VARIORUM_LOG = ON)")
+else()
+    message(STATUS "Building without logging statements (VARIORUM_LOG = OFF)")
+endif()
+
+#################
+# Documentation #
+#################
 if(BUILD_DOCS)
     message(STATUS "Building documentation (BUILD_DOCS == ON)")
+else()
+    message(STATUS "Building without documentation (BUILD_DOCS == OFF)")
 endif()
 
-################################
-# Shared vs Static Libs
-################################
+#########################
+# Shared vs Static Libs #
+#########################
 if(BUILD_SHARED_LIBS)
     message(STATUS "Building variorum as a shared library (BUILD_SHARED_LIBS == ON)")
 else()
