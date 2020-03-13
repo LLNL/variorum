53318174496500515d0433c7a545e68674ef4081
Stephanie Brink <brink2@llnl.gov>
2020-03-13 13:11:47 -0700

    update license year for 2020 (#44)

--- src/variorum/IBM/ibm_sensors.h
+++ src/variorum/IBM/ibm_sensors.h
@@ -1,4 +1,4 @@
-// Copyright 2019 Lawrence Livermore National Security, LLC and other
+// Copyright 2019-2020 Lawrence Livermore National Security, LLC and other
 // Variorum Project Developers. See the top-level LICENSE file for details.
 //
 // SPDX-License-Identifier: MIT
