cfb4a327356f441287e90896d012eb8d835408c9
Stephanie Brink <brink2@llnl.gov>
2020-03-20 15:51:51 -0700

    add c/c++ integration examples
    build systems:
    - make
    - cmake
    
    languages:
    - c
    - cxx

---
+++ src/examples/using-with-cmake/c/ex-print-power.c
@@ -0,0 +1,51 @@
+// Copyright 2019 Lawrence Livermore National Security, LLC and other
+// Variorum Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#include <stdio.h>
+
+#include <variorum.h>
+
+#ifdef SECOND_RUN
+static inline double do_work(int input)
+{
+    int i;
+    double result = (double)input;
+
+    for (i = 0; i < 100000; i++)
+    {
+        result += i*result;
+    }
+
+    return result;
+}
+#endif
+
+int main(int argc, char **argv)
+{
+    int ret;
+#ifdef SECOND_RUN
+    int i;
+    int size = 1E3;
+    double x = 0.0;
+#endif
+
+    ret = variorum_print_power();
+    if (ret != 0)
+    {
+        printf("Print power failed!\n");
+    }
+#ifdef SECOND_RUN
+    for (i = 0; i < size; i++)
+    {
+        x += do_work(i);
+    }
+    ret = variorum_print_power();
+    if (ret != 0)
+    {
+        printf("Print power failed!\n");
+    }
+#endif
+    return ret;
+}
---
+++ src/examples/using-with-make/cpp/ex-print-power.cpp
@@ -0,0 +1,51 @@
+// Copyright 2019 Lawrence Livermore National Security, LLC and other
+// Variorum Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+extern "C" {
+    #include <variorum.h>
+}
+
+#ifdef SECOND_RUN
+static inline double do_work(int input)
+{
+    double result = (double)input;
+
+    for (int i = 0; i < 100000; i++)
+    {
+        result += i*result;
+    }
+
+    return result;
+}
+#endif
+
+int main()
+{
+    int ret;
+#ifdef SECOND_RUN
+    int size = 1E3;
+    double x = 0.0;
+#endif
+
+    ret = variorum_print_power();
+    if (ret != 0)
+    {
+        std::cerr << "Print power failed!" << std::endl;
+    }
+#ifdef SECOND_RUN
+    for (int i = 0; i < size; i++)
+    {
+        x += do_work(i);
+    }
+    ret = variorum_print_power();
+    if (ret != 0)
+    {
+        std::cerr << "Print power failed!" << std::endl;
+    }
+#endif
+    return ret;
+}
