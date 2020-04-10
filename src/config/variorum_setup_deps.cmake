a09a225598d21327a8efc39e4237607638ff39a8
Stephanie Brink <brink2@llnl.gov>
2020-04-10 14:00:02 -0700

    fix CMake build for c example (#114)

--- src/config/variorum_setup_deps.cmake
+++ src/config/variorum_setup_deps.cmake
@@ -15,14 +15,3 @@ endif()
 if(NOT HWLOC_INCLUDE_LIB AND NOT HWLOC_LIB)
     message(FATAL_ERROR "Could not find HWLOC. HWLOC requires HWLOC_LIB and HWLOC_INCLUDE_LIB.")
 endif()
-
-if(NOT EXISTS ${HWLOC_DIR}/lib/cmake/hwloc.cmake)
-    message(FATAL_ERROR "Could not find HWLOC CMake include file (${HWLOC_DIR}/lib/cmake/hwloc.cmake)")
-endif()
-
-#######################################################################
-# Import HWLOC's CMake targets
-#######################################################################
-find_dependency(HWLOC REQUIRED
-                NO_DEFAULT_PATH
-                PATHS ${HWLOC_DIR}/lib/cmake)
