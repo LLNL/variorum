a7047d14974675c73bd80a810ff6acf73eadfc37
Stephanie Brink <brink2@llnl.gov>
2020-04-12 11:38:05 -0700

    update hwloc package dependency
    - set hwloc variables for using in other make/cmake build systems
    - remove hwloc as ExternalProject

--- src/CMake/thirdparty/SetupHwloc.cmake
+++ src/CMake/thirdparty/SetupHwloc.cmake
@@ -2,18 +2,20 @@
 if(HWLOC_DIR)
     message(STATUS "Looking for Hwloc using HWLOC_DIR = ${HWLOC_DIR}")
 
-    set(HWLOC_FOUND TRUE)
-    set(VARIORUM_HWLOC_DIR ${HWLOC_DIR})
-    set(HWLOC_INCLUDE_DIRS ${HWLOC_DIR}/include)
-    set(HWLOC_LIBRARY ${HWLOC_DIR}/lib/libhwloc.so)
+    set(HWLOC_FOUND TRUE CACHE INTERNAL "")
+    set(VARIORUM_HWLOC_DIR ${HWLOC_DIR} CACHE INTERNAL "")
+    set(HWLOC_DIR ${HWLOC_DIR} CACHE PATH "" FORCE)
+    set(HWLOC_INCLUDE_DIRS ${HWLOC_DIR}/include CACHE PATH "" FORCE)
+    set(HWLOC_LIBRARY ${HWLOC_DIR}/lib/libhwloc.so CACHE PATH "" FORCE)
+    include_directories(${HWLOC_INCLUDE_DIRS})
 
-    message(STATUS "FOUND hwloc at ${HWLOC_DIR}")
+    message(STATUS "FOUND hwloc")
     message(STATUS " [*] HWLOC_DIR = ${HWLOC_DIR}")
     message(STATUS " [*] HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
     message(STATUS " [*] HWLOC_LIBRARY = ${HWLOC_LIBRARY}")
 # If HWLOC_DIR not specified, then try to automatically find the HWLOC header
 # and library
-elseif(NOT HWLOC_DIR)
+elseif(NOT HWLOC_FOUND)
     find_path(HWLOC_INCLUDE_DIRS
         NAMES hwloc.h
     )
@@ -23,26 +25,17 @@ elseif(NOT HWLOC_DIR)
     )
 
     if(HWLOC_INCLUDE_DIRS AND HWLOC_LIBRARY)
-        set(HWLOC_FOUND TRUE)
+        set(HWLOC_FOUND TRUE CACHE INTERNAL "")
+        set(VARIORUM_HWLOC_DIR ${HWLOC_DIR} CACHE INTERNAL "")
+        set(HWLOC_DIR ${HWLOC_DIR} CACHE PATH "" FORCE)
+        set(HWLOC_INCLUDE_DIRS ${HWLOC_INCLUDE_DIRS} CACHE PATH "" FORCE)
+        set(HWLOC_LIBRARY ${HWLOC_LIBRARY} CACHE PATH "" FORCE)
+        include_directories(${HWLOC_INCLUDE_DIRS})
+
         message(STATUS "FOUND hwloc using find_library()")
         message(STATUS " [*] HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
         message(STATUS " [*] HWLOC_LIBRARY = ${HWLOC_LIBRARY}")
     endif()
-endif()
-
-# If HWLOC is still not found, then download and build HWLOC from source
-if(NOT HWLOC_INCLUDE_DIRS AND HWLOC_LIBRARY)
-    message(STATUS "Downloading and building hwloc from source")
-
-    ### Necessary for ExternalProject_Add
-    include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
-    include(../SetupExternalProjects.cmake)
-    message(STATUS "Adding external project: hwloc in ${HWLOC_DIR}")
-    message(STATUS " [*] HWLOC_DIR = ${HWLOC_DIR}")
-    message(STATUS " [*] HWLOC_INCLUDE_DIRS = ${HWLOC_INCLUDE_DIRS}")
-    message(STATUS " [*] HWLOC_LIBRARY = ${HWLOC_LIBRARY}")
-    set(HWLOC_FOUND TRUE)
-    set(VARIORUM_HWLOC_DIR ${HWLOC_DIR})
 endif()
 
 # Abort if all methods fail
--- src/config/variorum_setup_targets.cmake
+++ src/config/variorum_setup_targets.cmake
@@ -8,22 +8,31 @@ set_property(TARGET variorum::variorum
              INTERFACE_INCLUDE_DIRECTORIES "${VARIORUM_INSTALL_PREFIX}/include/")
 
 set_property(TARGET variorum::variorum
-             APPEND PROPERTY
-             INTERFACE_INCLUDE_DIRECTORIES "${VARIORUM_INSTALL_PREFIX}/include")
-
-set_property(TARGET variorum::variorum
              PROPERTY INTERFACE_LINK_LIBRARIES
              variorum)
 
-#set_property(TARGET variorum::variorum
-#             APPEND PROPERTY INTERFACE_LINK_LIBRARIES
-#             hwloc)
+if(TARGET hwloc::hwloc)
+    set_property(TARGET variorum::variorum
+                 APPEND PROPERTY INTERFACE_LINK_LIBRARIES
+                 hwloc::hwloc)
+else()
+    # if not, bottle hwloc
+    set_property(TARGET variorum::variorum
+                 APPEND PROPERTY
+                 INTERFACE_INCLUDE_DIRECTORIES ${HWLOC_INCLUDE_DIRS})
+
+    set_property(TARGET variorum::variorum
+                 APPEND PROPERTY INTERFACE_LINK_LIBRARIES
+                 hwloc)
+endif()
 
 if(NOT Variorum_FIND_QUIETLY)
+    message(STATUS "VARIORUM_VERSION        = ${VARIORUM_VERSION}")
     message(STATUS "VARIORUM_INSTALL_PREFIX = ${VARIORUM_INSTALL_PREFIX}")
     message(STATUS "VARIORUM_INCLUDE_DIRS   = ${VARIORUM_INCLUDE_DIRS}")
 
     set(_print_targets "")
+    set(_print_targets "variorum::variorum ")
     message(STATUS "Variorum imported targets: ${_print_targets}")
     unset(_print_targets)
 endif()
