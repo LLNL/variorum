b9952f5e6bbe7f8c58fe1552732ec543e3f3b0f2
Stephanie Brink <brink2@llnl.gov>
2021-07-13 18:28:01 -0700

    add host config file for comus and octomore (#190)

---
+++ host-configs/octomore-ubuntu.20.04.2.LTS-x86_64-gcc@9.3.0.cmake
@@ -0,0 +1,28 @@
+# Copyright 2021 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+# c compiler
+set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
+
+# cpp compiler
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+
+set(ENABLE_FORTRAN ON CACHE PATH "")
+
+set(USE_MSR_SAFE_BEFORE_1_5_0 OFF CACHE BOOL "")
+
+set(BUILD_DOCS OFF CACHE BOOL "")
+set(BUILD_TESTS OFF CACHE BOOL "")
+
+set(VARIORUM_WITH_AMD OFF CACHE BOOL "")
+set(VARIORUM_WITH_NVIDIA OFF CACHE BOOL "")
+set(VARIORUM_WITH_IBM OFF CACHE BOOL "")
+set(VARIORUM_WITH_INTEL ON CACHE BOOL "")
+
+# path to global hwloc install
+set(HWLOC_DIR "/usr/local/share/hwloc-1.11.7-install" CACHE PATH "")
+
+# path to global jansson install
+set(JANSSON_DIR "/usr/local/share/jansson-2.6-install" CACHE PATH "")
