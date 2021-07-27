9c01868cf689652f40564385a9e175489f72e61b
Stephanie Brink <brink2@llnl.gov>
2021-07-27 10:46:30 -0700

    update host configs
    Update host config files for octomore, lassen, and juno for upcoming ECP
    tutorial

--- host-configs/boilerplate.cmake
+++ host-configs/boilerplate.cmake
@@ -15,8 +15,19 @@ set(CMAKE_C_COMPILER "/path/to/c_compiler" CACHE PATH "")
 # cpp compiler
 set(CMAKE_CXX_COMPILER "/path/to/cxx_compiler" CACHE PATH "")
 
-# fortran compiler
+# build fortran example
+set(ENABLE_FORTRAN ON CACHE PATH "")
 set(CMAKE_Fortran_COMPILER  "/path/to/fortran_compiler" CACHE PATH "")
+
+# build for specific architecture
+set(VARIORUM_WITH_AMD OFF CACHE BOOL "")
+set(VARIORUM_WITH_ARM OFF CACHE BOOL "")
+set(VARIORUM_WITH_IBM OFF CACHE BOOL "")
+set(VARIORUM_WITH_INTEL ON CACHE BOOL "")
+set(VARIORUM_WITH_NVIDIA OFF CACHE BOOL "")
+
+set(VARIORUM_LOG OFF CACHE BOOL "")
+set(VARIORUM_DEBUG OFF CACHE BOOL "")
 
 # build documentation (depends on sphinx, doxygen, and breathe)
 set(BUILD_DOCS ON CACHE BOOL "")
@@ -24,11 +35,11 @@ set(BUILD_DOCS ON CACHE BOOL "")
 # build unit tests
 set(BUILD_TESTS ON CACHE BOOL "")
 
-# build for specific architecture
-set(VARIORUM_WITH_AMD OFF CACHE BOOL "")
-set(VARIORUM_WITH_NVIDIA OFF CACHE BOOL "")
-set(VARIORUM_WITH_IBM OFF CACHE BOOL "")
-set(VARIORUM_WITH_INTEL ON CACHE BOOL "")
-
 # SPHINX documentation building
 set("SPHINX_EXECUTABLE" "/path/to/sphinx-build" CACHE PATH "")
+
+# path to global hwloc install
+set(HWLOC_DIR "/path/to/hwloc-build" CACHE PATH "")
+
+# path to global jansson install
+set(JANSSON_DIR "/path/to/jansson-build" CACHE PATH "")
--- host-configs/quartz-toss_3_x86_64_ib-gcc@4.9.3.cmake
+++ host-configs/quartz-toss_3_x86_64_ib-gcc@4.9.3.cmake
@@ -18,9 +18,10 @@ set(BUILD_DOCS ON CACHE BOOL "")
 set(BUILD_TESTS OFF CACHE BOOL "")
 
 set(VARIORUM_WITH_AMD OFF CACHE BOOL "")
-set(VARIORUM_WITH_NVIDIA OFF CACHE BOOL "")
+set(VARIORUM_WITH_ARM OFF CACHE BOOL "")
 set(VARIORUM_WITH_IBM OFF CACHE BOOL "")
 set(VARIORUM_WITH_INTEL ON CACHE BOOL "")
+set(VARIORUM_WITH_NVIDIA OFF CACHE BOOL "")
 
 #SPHINX documentation building
 set("SPHINX_EXECUTABLE" "/collab/usr/gapps/python/build/spack-toss3.2/opt/spack/linux-rhel7-x86_64/gcc-4.9.3/py-sphinx-1.6.3-pd3yayuuejdpggkyqvhxbljwh4fhpo43/bin/sphinx-build" CACHE PATH "")
