03f228b6fd58af583aeb844b45d00c4830346ae9
jlnav <jnavarro@anl.gov>
2022-03-25 15:24:40 -0500

    initial commit, attempts to implement variorum_get_node_power_json for skylake

--- src/variorum/Intel/config_intel.c
+++ src/variorum/Intel/config_intel.c
@@ -188,6 +188,7 @@ int set_intel_func_ptrs(void)
         //g_platform.variorum_print_turbo = fm_06_55_get_turbo_status;
         //g_platform.variorum_enable_turbo = fm_06_55_enable_turbo;
         //g_platform.variorum_disable_turbo = fm_06_55_disable_turbo;
+        g_platform.variorum_get_node_power_json = fm_06_55_get_node_power_json;
         g_platform.variorum_poll_power = fm_06_55_poll_power;
         g_platform.variorum_monitoring = fm_06_55_monitoring;
         g_platform.variorum_cap_each_core_frequency_limit = fm_06_55_cap_frequency;
