d0a9043ef0699c7202a99f43d6bd62007bda3732
Barry Rountree <rountree@llnl.gov>
2022-05-23 16:28:15 -0700

    Adds host config file for lupine, LLNL power lab.

---
+++ host-configs/lupine-ubuntu.22.04.LTS-x86_64-gcc@11.2.0.cmake
@@ -0,0 +1,31 @@
+# Copyright 2019-2021 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+# c compiler
+set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
+
+# cpp compiler
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+
+# fortran compiler
+set(CMAKE_Fortran_COMPILER  "gfortran" CACHE PATH "")
+
+set(ENABLE_MPI OFF CACHE BOOL "")
+set(ENABLE_OPENMP OFF CACHE BOOL "")
+
+set(BUILD_DOCS OFF CACHE BOOL "")
+set(BUILD_TESTS ON CACHE BOOL "")
+
+set(VARIORUM_WITH_AMD OFF CACHE BOOL "")
+set(VARIORUM_WITH_ARM OFF CACHE BOOL "")
+set(VARIORUM_WITH_IBM OFF CACHE BOOL "")
+set(VARIORUM_WITH_INTEL ON CACHE BOOL "")
+set(VARIORUM_WITH_NVIDIA OFF CACHE BOOL "")
+
+# path to global hwloc install
+#set(HWLOC_DIR "/usr/local/share/hwloc-1.11.7-install" CACHE PATH "")
+
+# path to global jansson install
+#set(JANSSON_DIR "/usr/local/share/jansson-2.6-install" CACHE PATH "")
