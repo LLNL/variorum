7c6a4c90b5547b53afd4e312ff801d9569a4a557
chaz-browne <chazakilbrowne@gmail.com>
2022-06-14 10:30:48 -0700

    Adds -h,-v options to example.

--- src/examples/variorum-disable-turbo-example.c
+++ src/examples/variorum-disable-turbo-example.c
@@ -4,11 +4,31 @@
 // SPDX-License-Identifier: MIT
 
 #include <stdio.h>
-
+#include <variorum_config.h>
+#include <getopt.h>
 #include <variorum.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+    const char *usage = "Usage: %s [-h] \n";
+    int opt;
+    while ((opt = getopt(argc, argv, "hv")) != -1)
+    {
+        switch (opt)
+        {
+            case 'v':
+                printf(QuoteMacro(VARIORUM_VERSION)"\n");
+                return 0;
+                break;
+            case 'h':
+                printf(usage, argv[0]);
+                return 0;
+                break;
+            default:
+                fprintf(stderr, usage, argv[0]);
+                return -1;
+        }
+    }
     int ret;
 
     ret = variorum_disable_turbo();
