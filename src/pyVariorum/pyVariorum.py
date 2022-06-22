90ad1aa8c299aa98c63e709cfc7dadb68048b0b5
Tapasya Patki <patki1@llnl.gov>
2022-06-21 18:40:45 -0700

    Test capping API.

--- src/pyVariorum/pyVariorum.py
+++ src/pyVariorum/pyVariorum.py
@@ -7,15 +7,30 @@ class variorum:
             self.variorum_c = CDLL("libvariorum.so")
         except:
             print("\nVariorum shared library not found. Please check path to .so file")
-    
+
+    # Variorum Print Functions    
     def variorum_print_power(self):    
-        return self.variorum_c.variorum_print_power()
+        ret = self.variorum_c.variorum_print_power()
+        return ret
     
     def variorum_print_verbose_power(self):    
-        return self.variorum_c.variorum_print_verbose_power()
+        ret = self.variorum_c.variorum_print_verbose_power()
+        return ret
     
     def variorum_print_power_limit(self):    
-        return self.variorum_c.variorum_print_power_limit()
+        ret = self.variorum_c.variorum_print_power_limit()
+        return ret
     
     def variorum_print_verbose_power_limit(self):    
-        return self.variorum_c.variorum_print_verbose_power_limit()
+        ret = self.variorum_c.variorum_print_verbose_power_limit()
+        return ret
+
+    # Variorum Cap Functions    
+    def variorum_cap_best_effort_node_power_limit(self, node_power_limit):
+        ret = self.variorum_c.variorum_cap_best_effort_node_power_limit(node_power_limit)
+        return ret
+
+    # Variorum JSON Functions    
+#    def variorum_get_node_power_json(self, get_power_obj):
+#        ret = self.variorum_c.variorum_get_node_power_json(byref(get_power_obj))
+#        return ret
--- src/pyVariorum/runtests.py
+++ src/pyVariorum/runtests.py
@@ -13,6 +13,15 @@ def runtests(v):
     print("\n=== Running Variorum Print Power Limit (Verbose):")
     v.variorum_print_verbose_power_limit()
 
+    print("\n=== Running Variorum Cap Best Effort Node Power Limit:")
+    v.variorum_cap_best_effort_node_power_limit(2000)
+    print("\n######## Verifying with Variorum Print Power Limit:")
+    v.variorum_print_power_limit()
+
+#def runJSONTests(v)
+#    print("\n=== Running Variorum Cap Best Effort Node Power Limit:")
+#    v.variorum_get_node_power_json(my_power_obj)
+
 if __name__ == "__main__":
     v = pyVariorum.variorum()
     runtests(v)
