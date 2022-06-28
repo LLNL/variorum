b31173e36a7eed0a3df995f8533e29bde52fd3d8
Stephanie Brink <brink2@llnl.gov>
2022-06-27 19:47:40 -0700

    fix astyle errors

--- src/examples/variorum-get-topology-info-example.c
+++ src/examples/variorum-get-topology-info-example.c
@@ -9,45 +9,45 @@
 
 int main(int argc, char **argv)
 {
-    int num_sockets, num_cores, num_threads; 
+    int num_sockets, num_cores, num_threads;
 
-    // Demonstrates usage of Variorum's advanced API for obtaining topology information. 
+    // Demonstrates usage of Variorum's advanced API for obtaining topology information.
     printf("This example demonstrates the use of variorum_topology.h API \n"
-           "to obtain basic information from the underlying architecture.\n"); 
+           "to obtain basic information from the underlying architecture.\n");
 
     num_sockets =  variorum_get_num_sockets();
 
-    if (num_sockets > 0) 
+    if (num_sockets > 0)
     {
-        printf("Number of sockets on this hardware: %d\n", num_sockets); 
+        printf("Number of sockets on this hardware: %d\n", num_sockets);
     }
     else
     {
         printf("HWLOC returned an invalid number of sockets (<=0)."
-               "Variorum does not handle this case. \n"); 
+               "Variorum does not handle this case. \n");
     }
 
-    num_cores = variorum_get_num_cores(); 
+    num_cores = variorum_get_num_cores();
 
-    if (num_cores > 0) 
+    if (num_cores > 0)
     {
-        printf("Number of cores on this hardware: %d\n", num_cores); 
+        printf("Number of cores on this hardware: %d\n", num_cores);
     }
     else
     {
         printf("HWLOC returned an invalid number of cores (<=0)."
-               "Variorum does not handle this case. \n"); 
+               "Variorum does not handle this case. \n");
     }
 
-    num_threads = variorum_get_num_threads(); 
-    
-    if (num_threads > 0) 
+    num_threads = variorum_get_num_threads();
+
+    if (num_threads > 0)
     {
-        printf("Number of threads on this hardware: %d\n", num_threads); 
+        printf("Number of threads on this hardware: %d\n", num_threads);
     }
     else
     {
         printf("HWLOC returned an invalid number of threads (<=0)."
-               "Variorum does not handle this case. \n"); 
+               "Variorum does not handle this case. \n");
     }
 }
