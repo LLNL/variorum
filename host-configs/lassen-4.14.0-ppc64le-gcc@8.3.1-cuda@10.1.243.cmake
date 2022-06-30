4c6ff886b20cbb06a4541fffc64a1bb91741fef0
Stephanie Labasan Brink <brink2@llnl.gov>
2022-06-30 08:34:53 -0700

    update lassen host configs (#274)
    - if BUILD_TESTS=ON, then gcc>5 to support newer Google Test
    - disable tests in host config for ray, rhetoric, and thompson

--- host-configs/lassen-4.14.0-ppc64le-gcc@8.3.1-cuda@10.1.243.cmake
+++ host-configs/lassen-4.14.0-ppc64le-gcc@8.3.1-cuda@10.1.243.cmake
@@ -4,16 +4,16 @@
 # SPDX-License-Identifier: MIT
 
 # c compiler
-set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/gcc" CACHE PATH "")
 
 # cpp compiler
-set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/g++" CACHE PATH "")
 
 set(ENABLE_MPI OFF CACHE BOOL "")
 set(ENABLE_OPENMP ON CACHE BOOL "")
 
 set(BUILD_DOCS OFF CACHE BOOL "")
-set(BUILD_TESTS ON CACHE BOOL "")
+set(BUILD_TESTS OFF CACHE BOOL "")
 
 set(VARIORUM_WITH_AMD OFF CACHE BOOL "")
 set(VARIORUM_WITH_ARM OFF CACHE BOOL "")
--- host-configs/lassen-4.14.0-ppc64le-gcc@8.3.1.cmake
+++ host-configs/lassen-4.14.0-ppc64le-gcc@8.3.1.cmake
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: MIT
 
 # c compiler
-set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/gcc" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/gcc" CACHE PATH "")
 
 # cpp compiler
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/g++" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/g++" CACHE PATH "")
 
 # fortran compiler
-set(CMAKE_Fortran_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/gfortran" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/gfortran" CACHE PATH "")
 
 set(ENABLE_MPI OFF CACHE BOOL "")
 set(ENABLE_OPENMP ON CACHE BOOL "")
@@ -25,7 +25,7 @@ set(VARIORUM_LOG OFF CACHE BOOL "")
 set(VARIORUM_DEBUG OFF CACHE BOOL "")
 
 set(BUILD_DOCS OFF CACHE BOOL "")
-set(BUILD_TESTS OFF CACHE BOOL "")
+set(BUILD_TESTS ON CACHE BOOL "")
 
 #SPHINX documentation building
 set("SPHINX_EXECUTABLE" "/collab/usr/gapps/python/build/spack-coralea/opt/spack/linux-rhel7-ppc64le/gcc-4.8.5/py-sphinx-1.6.1-txjs2mkq6eoq6eteawczvpnnfopaychw/bin/sphinx-build" CACHE PATH "")
