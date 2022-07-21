e0c187c9adf421e9b22d53c8069bc040283882a5
Tapasya Patki <patki1@llnl.gov>
2022-07-20 19:20:30 -0700

    Formatting fixes for python files

--- src/pyVariorum/examples/runtests.py
+++ src/pyVariorum/examples/runtests.py
@@ -1,7 +1,7 @@
-import ctypes
-from ctypes import *
+from ctypes import create_unicode_buffer, addressof, c_char_p
 import pyVariorum
-    
+
+
 def runtests(v):
     print("\n=== Running Variorum Print Power:")
     v.variorum_print_power()
@@ -17,13 +17,15 @@ def runtests(v):
     print("\n######## Verifying with Variorum Print Power Limit:")
     v.variorum_print_power_limit()
 
+
 def runJSONtests(v):
     print("\n=== Running Variorum JSON Get Node Power:")
-    jsonbuf = ctypes.create_unicode_buffer(1000)
+    jsonbuf = create_unicode_buffer(1000)
     bufptr = c_char_p(addressof(jsonbuf))
     v.variorum_get_node_power_json(bufptr)
     print(bufptr.value.decode('utf-8'))
     # print(bufptr.value)
+
 
 if __name__ == "__main__":
     v = pyVariorum.variorum()
--- src/pyVariorum/pyVariorum/pyVariorum.py
+++ src/pyVariorum/pyVariorum/pyVariorum.py
@@ -1,14 +1,15 @@
-from ctypes import *
+from ctypes import c_int, c_char_p, CDLL, POINTER
+
 
 class variorum:
 
     def __init__(self):
         try:
             self.variorum_c = CDLL("libvariorum.so")
-        except:
+        except Exception:
             print("\nVariorum shared library not found. Please check path to .so file")
 
-        # Variorum Print Functions    
+        # Variorum Print Functions
         self.variorum_print_power = self.variorum_c.variorum_print_power
         self.variorum_print_power.restype = c_int
 
@@ -17,13 +18,12 @@ class variorum:
 
         self.variorum_print_verbose_power_limit = self.variorum_c.variorum_print_verbose_power_limit
         self.variorum_print_verbose_power_limit.restype = c_int
-   
+
         self.variorum_cap_best_effort_node_power_limit = self.variorum_c.variorum_cap_best_effort_node_power_limit
         self.variorum_cap_best_effort_node_power_limit.argtypes = [c_int]
         self.variorum_cap_best_effort_node_power_limit.restype = c_int
 
-        # Variorum JSON Functions    
+        # Variorum JSON Functions
         self.variorum_get_node_power_json = self.variorum_c.variorum_get_node_power_json
         self.variorum_get_node_power_json.argtypes = [POINTER(c_char_p)]
         self.variorum_get_node_power_json.restype = c_int
-
