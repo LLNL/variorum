07e3677a9ea2222aa7fe2ff3bf8aa24f916d8bc0
Barry Rountree <rountree@llnl.gov>
2022-07-21 11:38:46 -0700

    Fixes coding typo.

--- src/variorum/Intel/Intel_06_4F.c
+++ src/variorum/Intel/Intel_06_4F.c
@@ -476,7 +476,7 @@ int fm_06_4f_get_node_power_domain_info_json(char **get_domain_obj_str)
     json_t *get_domain_obj = json_object();
     json_get_power_domain_info(get_domain_obj, msrs.msr_pkg_power_info,
                                msrs.msr_dram_power_info,
-                               msrs.msr_rapl_power_unit, msrs.msr_pkg_power_limit;
+                               msrs.msr_rapl_power_unit, msrs.msr_pkg_power_limit);
 
     *get_domain_obj_str = json_dumps(get_domain_obj, 0);
     json_decref(get_domain_obj);
