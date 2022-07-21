e446efbe243c966cb90c3178c3f1f49509751088
Tapasya Patki <patki1@llnl.gov>
2022-07-20 18:38:53 -0700

    Add error checks with RSMI_STATUS_SUCCESS for all RSMI APIS

--- src/variorum/AMD_GPU/power_features.c
+++ src/variorum/AMD_GPU/power_features.c
@@ -26,12 +26,24 @@ void get_power_data(int chipid, int total_sockets, int verbose, FILE *output)
     gethostname(hostname, 1024);
 
     ret = rsmi_init(0);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not initialize RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
+
     ret = rsmi_num_monitor_devices(&num_devices);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not get number of GPU devices",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 
     gpus_per_socket = num_devices / total_sockets;
-
-    // printf("\nSanity check, num_devices: %d, total_sockets: %d, gpus_per_sokcet: %d",
-    //    num_devices, total_sockets, gpus_per_socket);
 
     if (!init)
     {
@@ -52,9 +64,14 @@ void get_power_data(int chipid, int total_sockets, int verbose, FILE *output)
         uint64_t pwr_val = -1;
         double pwr_val_flt = -1.0;
 
-        // printf("\nSanity check. ChipID: %d, dev_id: %d", chipid, i);
-
         ret = rsmi_dev_power_ave_get(i, 0, &pwr_val);
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
 
         pwr_val_flt = (double)(pwr_val / (1000 * 1000)); // Convert to Watts.
 
@@ -75,6 +92,13 @@ void get_power_data(int chipid, int total_sockets, int verbose, FILE *output)
 
     }
     ret = rsmi_shut_down();
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not shutdown RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 }
 
 void get_power_limit_data(int chipid, int total_sockets, int verbose,
@@ -91,12 +115,24 @@ void get_power_limit_data(int chipid, int total_sockets, int verbose,
     gethostname(hostname, 1024);
 
     ret = rsmi_init(0);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not initialize RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
+
     ret = rsmi_num_monitor_devices(&num_devices);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not get number of GPU devices",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 
     gpus_per_socket = num_devices / total_sockets;
-
-    // printf("\nSanity check, num_devices: %d, total_sockets: %d, gpus_per_sokcet: %d",
-    //    num_devices, total_sockets, gpus_per_socket);
 
     if (!init)
     {
@@ -118,10 +154,23 @@ void get_power_limit_data(int chipid, int total_sockets, int verbose,
         uint64_t pwr_val = -1, pwr_min = -1, pwr_max = -1;
         double pwr_val_flt = -1.0;
 
-        // printf("\nSanity check. ChipID: %d, dev_id: %d", chipid, i);
-
         ret = rsmi_dev_power_cap_get(i, 0, &pwr_val);
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
+
         ret = rsmi_dev_power_cap_range_get(i, 0, &pwr_max, &pwr_min);
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
 
         pwr_val_flt = (double)(pwr_val / (1000 * 1000)); // Convert to Watts.
         pwr_max = (pwr_max / (1000 * 1000)); // Convert to Watts.
@@ -145,6 +194,13 @@ void get_power_limit_data(int chipid, int total_sockets, int verbose,
 
     }
     ret = rsmi_shut_down();
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not shutdown RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 }
 
 void get_thermals_data(int chipid, int total_sockets, int verbose, FILE *output)
@@ -160,12 +216,24 @@ void get_thermals_data(int chipid, int total_sockets, int verbose, FILE *output)
     gethostname(hostname, 1024);
 
     ret = rsmi_init(0);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not initialize RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
+
     ret = rsmi_num_monitor_devices(&num_devices);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not get number of GPU devices",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 
     gpus_per_socket = num_devices / total_sockets;
-
-    // printf("\nSanity check, num_devices: %d, total_sockets: %d, gpus_per_sokcet: %d",
-    //    num_devices, total_sockets, gpus_per_socket);
 
     if (!init)
     {
@@ -186,10 +254,15 @@ void get_thermals_data(int chipid, int total_sockets, int verbose, FILE *output)
         uint64_t temp_val = -1;
         double temp_val_flt = -1.0;
 
-        // printf("\nSanity check. ChipID: %d, dev_id: %d", chipid, i);
-
         ret = rsmi_dev_temp_metric_get(i, RSMI_TEMP_TYPE_EDGE, RSMI_TEMP_CURRENT,
                                        &temp_val);
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
 
         temp_val_flt = (double)(temp_val / (1000)); // Convert to Celcius.
 
@@ -210,6 +283,13 @@ void get_thermals_data(int chipid, int total_sockets, int verbose, FILE *output)
 
     }
     ret = rsmi_shut_down();
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not shutdown RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 }
 
 void get_clocks_data(int chipid, int total_sockets, int verbose, FILE *output)
@@ -225,12 +305,24 @@ void get_clocks_data(int chipid, int total_sockets, int verbose, FILE *output)
     gethostname(hostname, 1024);
 
     ret = rsmi_init(0);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not initialize RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
+
     ret = rsmi_num_monitor_devices(&num_devices);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not get number of GPU devices",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 
     gpus_per_socket = num_devices / total_sockets;
-
-    // printf("\nSanity check, num_devices: %d, total_sockets: %d, gpus_per_sokcet: %d",
-    //    num_devices, total_sockets, gpus_per_socket);
 
     if (!init)
     {
@@ -252,10 +344,23 @@ void get_clocks_data(int chipid, int total_sockets, int verbose, FILE *output)
         rsmi_frequencies_t f_sys, f_mem;
         uint32_t f_sys_val, f_mem_val;
 
-        // printf("\nSanity check. ChipID: %d, dev_id: %d", chipid, i);
-
         ret = rsmi_dev_gpu_clk_freq_get(i, RSMI_CLK_TYPE_SYS, &f_sys);
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
+
         ret = rsmi_dev_gpu_clk_freq_get(i, RSMI_CLK_TYPE_MEM, &f_mem);
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
 
         f_sys_val = f_sys.frequency[f_sys.current] / (1000 * 1000); // Convert to MHz
         f_mem_val = f_mem.frequency[f_mem.current] / (1000 * 1000); // Convert to MHz
@@ -277,6 +382,13 @@ void get_clocks_data(int chipid, int total_sockets, int verbose, FILE *output)
 
     }
     ret = rsmi_shut_down();
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not shutdown RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 }
 
 void get_gpu_utilization_data(int chipid, int total_sockets, int verbose,
@@ -293,12 +405,24 @@ void get_gpu_utilization_data(int chipid, int total_sockets, int verbose,
     gethostname(hostname, 1024);
 
     ret = rsmi_init(0);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not initialize RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
+
     ret = rsmi_num_monitor_devices(&num_devices);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not get number of GPU devices",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 
     gpus_per_socket = num_devices / total_sockets;
-
-    // printf("\nSanity check, num_devices: %d, total_sockets: %d, gpus_per_sokcet: %d",
-    //    num_devices, total_sockets, gpus_per_socket);
 
     if (!init)
     {
@@ -324,10 +448,14 @@ void get_gpu_utilization_data(int chipid, int total_sockets, int verbose,
         // This requests ROCM GPU Memory Activity.
         util_ctr[1].type = RSMI_COARSE_GRAIN_MEM_ACTIVITY;
 
-        // printf("\nSanity check. ChipID: %d, dev_id: %d", chipid, i);
-
         ret = rsmi_utilization_count_get(i, util_ctr, 2, &ts);
-
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
 
         if (verbose == 1)
         {
@@ -346,5 +474,11 @@ void get_gpu_utilization_data(int chipid, int total_sockets, int verbose,
 
     }
     ret = rsmi_shut_down();
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not shutdown RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
 }
-
