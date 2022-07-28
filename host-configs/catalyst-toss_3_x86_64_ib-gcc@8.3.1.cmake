752805521ca5fac6cc9c487d974255649288f605
Stephanie Labasan Brink <brink2@llnl.gov>
2022-07-28 00:27:54 -0700

    bump gcc version for quartz and catalyst host configs (#306)
    BUILD_TESTS=ON requires gcc>=5

--- host-configs/catalyst-toss_3_x86_64_ib-gcc@8.3.1.cmake
+++ host-configs/catalyst-toss_3_x86_64_ib-gcc@8.3.1.cmake
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: MIT
 
 # c compiler
-set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/gcc" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/gcc" CACHE PATH "")
 
 # cpp compiler
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/g++" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/g++" CACHE PATH "")
 
 # fortran compiler
-set(CMAKE_Fortran_COMPILER  "/usr/tce/packages/gcc/gcc-4.9.3/bin/gfortran" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER  "/usr/tce/packages/gcc/gcc-8.3.1/bin/gfortran" CACHE PATH "")
 
 set(USE_MSR_SAFE_BEFORE_1_5_0 ON CACHE BOOL "")
 
