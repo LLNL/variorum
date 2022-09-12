489e43adce8919a39f1fbb91e9d253bcf513e9e2
Stephanie Brink <brink2@llnl.gov>
2022-09-12 15:32:46 -0700

    create remaining examples, remove main script

---
+++ src/examples/python-examples/variorum-cap-gpu-power-ratio-python-example.py
@@ -0,0 +1,11 @@
+# Copyright 2019-2022 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from pyVariorum import variorum
+
+if __name__ == "__main__":
+    v = variorum.variorum()
+    print("\n=== Running Variorum Cap GPU Power Ratio (input = 80%):")
+    v.variorum_cap_gpu_power_ratio(80)
---
+++ src/examples/python-examples/variorum-print-frequency-python-example.py
@@ -0,0 +1,11 @@
+# Copyright 2019-2022 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from pyVariorum import variorum
+
+if __name__ == "__main__":
+    v = variorum.variorum()
+    print("\n=== Running Variorum Print Frequency:")
+    v.variorum_print_frequency()
