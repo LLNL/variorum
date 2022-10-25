59b39869c3adb44cfe0216d62b59150f0f1f8002
Stephanie Brink <brink2@llnl.gov>
2022-10-24 22:50:55 -0700

    refactor formatting scripts
    - update PR template checklist
    - create new top-level script to launch checks for cpp/c, rst, and python files

--- scripts/check-code-format.sh
+++ scripts/check-code-format.sh
@@ -8,30 +8,43 @@
 # check format for all *.cpp, *.h, and *.c files in src/ directory
 #   exclude files in thirdparty_builtin/ (gtest source files)
 #   exclude files in fortran/ (EP source files)
-FILES=$(find src -type f  \( -name "*.[ch]" -o -name *.cpp \) \
-        | grep -v thirdparty_builtin \
-        | grep -v fortran)
+function check_format_files_cpp_c_h {
+    if ! command -v astyle &> /dev/null; then
+        echo -e "'astyle' not found: see https://astyle.sourceforge.net/ for installation info"
+        exit
+    fi
+    echo -e "Checking format of cpp/c/h files using astyle"
 
-astyle --errors-to-stdout \
-       --preserve-date \
-       --style=allman \
-       --indent=spaces=4 \
-       --convert-tabs \
-       --attach-extern-c \
-       --indent-col1-comments \
-       --min-conditional-indent=0 \
-       --max-instatement-indent=40 \
-       --pad-oper \
-       --pad-header \
-       --unpad-paren \
-       --align-pointer=name \
-       --align-reference=name \
-       --break-closing-brackets \
-       --keep-one-line-blocks \
-       --keep-one-line-statements \
-       --max-code-length=80 \
-       --break-after-logical \
-       --indent-switches \
-       ${FILES}
+    ./scripts/check-cpp-c-format.sh
+}
+
+function check_format_files_rst {
+    if ! command -v rstfmt &> /dev/null; then
+        echo -e "'rstfmt' not found: this package can be installed using pip"
+        exit
+    fi
+    echo -e "Checking format of rst documentation using rstfmt"
+
+    ./scripts/check-rst-format.sh
+}
+
+function check_format_files_python {
+    if ! command -v flake8 &> /dev/null; then
+        echo -e "'flake8' not found: this package can be installed using pip"
+        exit
+    fi
+    if ! command -v black &> /dev/null; then
+        echo -e "'black' not found: this package can be installed using pip"
+        exit
+    fi
+    echo -e "Checking format of python files using flake and black"
+
+    flake8
+    black --diff --check --exclude "/(src/thirdparty_builtin/googletest|build|src/docs)/" .
+}
+
+check_format_files_cpp_c_h
+check_format_files_rst
+check_format_files_python
 
 #
