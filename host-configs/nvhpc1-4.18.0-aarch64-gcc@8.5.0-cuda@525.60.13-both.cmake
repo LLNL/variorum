66ffc62c7923e32bf01dcb6ea954d5d06159fbe7
Aniruddha Marathe <marathe1@llnl.gov>
2023-01-10 18:17:47 -0800

    Hostfile for nvhpc1.

---
+++ host-configs/nvhpc1-4.18.0-aarch64-gcc@8.5.0-cuda@525.60.13-both.cmake
@@ -0,0 +1,27 @@
+# Copyright 2019-2022 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+# c compiler
+set(CMAKE_C_COMPILER "/usr/bin/gcc" CACHE PATH "")
+
+# cpp compiler
+set(CMAKE_CXX_COMPILER "/usr/bin/g++" CACHE PATH "")
+
+set(BUILD_DOCS OFF CACHE BOOL "")
+set(BUILD_TESTS OFF CACHE BOOL "")
+
+set(VARIORUM_WITH_ARM_CPU ON CACHE BOOL "")
+set(VARIORUM_WITH_NVIDIA_GPU ON CACHE BOOL "")
+set(VARIORUM_WITH_AMD_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_AMD_GPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_IBM_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_INTEL_CPU OFF CACHE BOOL "")
+
+set(CMAKE_SHARED_LINKER_FLAGS "-L/usr/local/lib -L/opt/nvidia/hpc_sdk/Linux_aarch64/22.1/cuda/11.5/targets/sbsa-linux/lib/stubs/ -lnvidia-ml" CACHE PATH "")
+
+# path to global hwloc install
+set(HWLOC_DIR "/usr/local" CACHE PATH "")
+
+set(NVML_DIR "/opt/nvidia/hpc_sdk/Linux_aarch64/22.1/cuda/11.5/targets/sbsa-linux/" CACHE PATH "")
