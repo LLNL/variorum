dd277c4f38b1d6f9a4f8e3d2df74d380362866d6
Tapasya Patki <patki1@llnl.gov>
2023-02-16 21:29:12 -0800

    Tioga host configs based on common install (#363)

---
+++ host-configs/tioga-4.18.0toss.t4.x86_64-gcc@10.3.1-rocm@5.2.cmake
@@ -0,0 +1,33 @@
+# Copyright 2019-2022 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+# c compiler
+set(CMAKE_C_COMPILER "/usr/tce/packages/gcc-tce/gcc-10.3.1/bin/gcc" CACHE PATH "")
+
+# cpp compiler
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc-tce/gcc-10.3.1/bin/g++" CACHE PATH "")
+
+# fortran compiler
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/gcc-tce/gcc-10.3.1/bin/gfortran" CACHE PATH "")
+
+set(ENABLE_MPI OFF CACHE BOOL "")
+set(ENABLE_OPENMP OFF CACHE BOOL "")
+
+set(BUILD_DOCS OFF CACHE BOOL "")
+set(BUILD_TESTS OFF CACHE BOOL "")
+
+set(VARIORUM_DEBUG OFF CACHE BOOL "")
+
+set(VARIORUM_WITH_AMD_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_AMD_GPU ON CACHE BOOL "")
+set(VARIORUM_WITH_ARM_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_IBM_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_INTEL_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_INTEL_GPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_NVIDIA_GPU OFF CACHE BOOL "")
+
+#ROCm setup
+set(CMAKE_SHARED_LINKER_FLAGS "-L/opt/rocm-5.2.0/lib -lrocm_smi64" CACHE PATH "")
+set(ROCM_DIR "/opt/rocm-5.2.0/" CACHE PATH "")
