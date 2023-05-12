2a4289c8738ab7fae555ff8e6efa7540f28ae4fd
Tapasya Patki <patki1@llnl.gov>
2023-05-12 16:30:15 -0700

    New logo. (#405)
    * New logo with linen background color.

--- src/docs/sphinx/conf.py
+++ src/docs/sphinx/conf.py
@@ -111,7 +111,7 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {'style_nav_header_background': 'linen','logo_only': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -125,7 +125,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "../../../images/Variorum_3in_1in.png"
+html_logo = "../../../images/variorum-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
