36f958ecec72141f27bc5d5b99d060797cb008fd
Tapasya Patki <patki1@llnl.gov>
2023-05-12 14:49:23 -0700

    New logo.

--- src/docs/sphinx/conf.py
+++ src/docs/sphinx/conf.py
@@ -125,7 +125,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "../../../images/Variorum_3in_1in.png"
+html_logo = "../../../images/variorum-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
