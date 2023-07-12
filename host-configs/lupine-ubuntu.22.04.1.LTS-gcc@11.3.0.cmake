385fbfa08fc5f20dd4deae3c6afcb06de33b0264
Duncan McFarlane <dmcf2@pdx.edu>
2023-07-12 10:26:47 -0700

    Created lupine host-config

---
+++ host-configs/lupine-ubuntu.22.04.1.LTS-gcc@11.3.0.cmake
@@ -0,0 +1,42 @@
+# Copyright 2019-2023 Lawrence Livermore National Security, LLC and other
+# Variorum Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+# c compiler
+set(CMAKE_C_COMPILER "/usr/bin/gcc" CACHE PATH "")
+
+# cpp compiler
+set(CMAKE_CXX_COMPILER "/usr/bin/g++" CACHE PATH "")
+
+#fortran compiler
+set(CMAKE_Fortran_COMPILER  "/usr/bin/gfortran" CACHE PATH "")
+
+set(ENABLE_FORTRAN ON CACHE PATH "")
+set(ENABLE_MPI ON CACHE BOOL "")
+set(ENABLE_OPENMP ON CACHE BOOL "")
+set(ENABLE_CPRINTF ON CACHE BOOL "")
+
+set(VARIORUM_WITH_AMD_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_AMD_GPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_ARM_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_IBM_CPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_INTEL_CPU ON CACHE BOOL "")
+set(VARIORUM_WITH_INTEL_GPU OFF CACHE BOOL "")
+set(VARIORUM_WITH_NVIDIA_GPU OFF CACHE BOOL "")
+
+set(USE_MSR_SAFE_BEFORE_1_5_0 OFF CACHE BOOL "")
+
+set(VARIORUM_DEBUG OFF CACHE BOOL "")
+
+set(BUILD_DOCS OFF CACHE BOOL "")
+set(BUILD_TESTS OFF CACHE BOOL "")
+
+# path to hwloc install
+set(HWLOC_DIR "/usr/include/hwloc/" CACHE PATH "")
+
+# Path to JANSSON build
+set(JANSSON_DIR "/usr/include/" CACHE PATH "")
+
+# Path to cprintf install
+set(CPRINTF_DIR "/usr/include/cprintf/" CACHE PATH "")
