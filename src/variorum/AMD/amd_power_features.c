510d63e2eaaf47c7a9576d37f8cc9b96653cc819
Duncan McFarlane <dmcf2@pdx.edu>
2023-07-12 12:18:51 -0700

    Update amd_power_features to use Cprintf

--- src/variorum/AMD/amd_power_features.c
+++ src/variorum/AMD/amd_power_features.c
@@ -15,6 +15,10 @@
 #include "msr_core.h"
 #include "amd_power_features.h"
 
+#ifdef CPRINTF_FOUND
+#include <cprintf.h>
+#endif
+
 static void create_rapl_data_batch(struct rapl_data *rapl,
                                    off_t msr_core_energy_status)
 {
@@ -145,10 +149,24 @@ int print_energy_data(FILE *writedest, off_t msr_rapl_unit,
 
     get_rapl_unit(msr_rapl_unit, &val);
 
-    fprintf(writedest, " Core   | Energy (J)   |\n");
+    #ifdef CPRINTF_FOUND
+        cfprintf(writedest, "%s  | %s  |\n", "Core", "Energy (J)");
+    #else
+        fprintf(writedest, " Core   | Energy (J)   |\n");
+    #endif
+
     for (i = 0; i < (int)ncores; i++)
-    {
-        fprintf(writedest, "%6d  | %10f  |\n", i, (*rapl->core_bits[i]) * val);
+    {   
+        #ifdef CPRINTF_FOUND
+            cprintf(writedest, "%d  | %f  |\n", i, (*rapl->core_bits[i]) * val);
+        #else
+            fprintf(writedest, "%6d  | %10f  |\n", i, (*rapl->core_bits[i]) * val);
+        #endif
     }
+
+    #ifdef CPRINTF_FOUND
+        cflush();
+    #endif
+    
     return 0;
 }
