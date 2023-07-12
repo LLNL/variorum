96a47444af52926299937216975127c293b31570
Duncan McFarlane <dmcf2@pdx.edu>
2023-07-12 11:09:14 -0700

    Updated cprintf Directory (Again whoops)

--- host-configs/lupine-ubuntu.22.04.1.LTS-gcc@11.3.0.cmake
+++ host-configs/lupine-ubuntu.22.04.1.LTS-gcc@11.3.0.cmake
@@ -39,4 +39,4 @@ set(HWLOC_DIR "/usr/include/hwloc/" CACHE PATH "")
 set(JANSSON_DIR "/usr/include/" CACHE PATH "")
 
 # Path to cprintf install
-set(CPRINTF_DIR "/home/$ENV{USER}/cprintf/" CACHE PATH "")
+set(CPRINTF_DIR "/home/$ENV{USER}/libjustify/" CACHE PATH "")
