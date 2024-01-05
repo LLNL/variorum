dd82fa77f6c65a3670ab9af10a7f009614936fa6
Tapasya Patki <patki1@llnl.gov>
2024-01-05 11:29:13 -0800

    AMD WIP changes

--- src/variorum/AMD_GPU/amd_gpu_power_features.c
+++ src/variorum/AMD_GPU/amd_gpu_power_features.c
@@ -553,6 +553,94 @@ void cap_each_gpu_power_limit(int chipid, int total_sockets,
                                __LINE__);
     }
 }
+void get_json_power_data(json_t *get_power_obj)
+{
+    rsmi_status_t ret;
+    uint32_t num_devices;
+    int gpus_per_socket;
+    char hostname[1024];
+    static int init = 0;
+    static struct timeval start;
+    struct timeval now;
+
+    gethostname(hostname, 1024);
+
+    ret = rsmi_init(0);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not initialize RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+        exit(-1);
+    }
+
+    ret = rsmi_num_monitor_devices(&num_devices);
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not get number of GPU devices",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
+
+    gpus_per_socket = num_devices / total_sockets;
+
+    if (!init)
+    {
+        init = 1;
+        gettimeofday(&start, NULL);
+        if (verbose == 0)
+        {
+            fprintf(output,
+                    "_AMD_GPU_POWER_USAGE Host Socket DeviceID Power Timestamp_sec\n");
+        }
+    }
+
+    gettimeofday(&now, NULL);
+
+    for (int i = chipid * gpus_per_socket;
+         i < (chipid + 1) * gpus_per_socket; i++)
+    {
+        uint64_t pwr_val = -1;
+        double pwr_val_flt = -1.0;
+
+        ret = rsmi_dev_power_ave_get(i, 0, &pwr_val);
+        if (ret != RSMI_STATUS_SUCCESS)
+        {
+            variorum_error_handler("RSMI API was not successful",
+                                   VARIORUM_ERROR_PLATFORM_ENV,
+                                   getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                                   __LINE__);
+        }
+
+        pwr_val_flt = (double)(pwr_val / (1000 * 1000)); // Convert to Watts.
+
+        if (verbose == 1)
+        {
+            fprintf(output,
+                    "_AMD_GPU_POWER_USAGE Host: %s, Socket: %d, DeviceID: %d,"
+                    " Power: %0.2lf W, Timestamp: %lf sec\n",
+                    hostname, chipid, i, pwr_val_flt,
+                    (now.tv_usec - start.tv_usec) / 1000000.0);
+        }
+        else
+        {
+            fprintf(output, "_AMD_GPU_POWER_USAGE %s %d %d %0.2lf %lf\n",
+                    hostname, chipid, i, pwr_val_flt,
+                    (now.tv_usec - start.tv_usec) / 1000000.0);
+        }
+
+    }
+    ret = rsmi_shut_down();
+    if (ret != RSMI_STATUS_SUCCESS)
+    {
+        variorum_error_handler("Could not shutdown RSMI",
+                               VARIORUM_ERROR_PLATFORM_ENV,
+                               getenv("HOSTNAME"), __FILE__, __FUNCTION__,
+                               __LINE__);
+    }
+}
 
 void get_json_power_data(json_t *get_power_obj)
 {
@@ -560,8 +648,11 @@ void get_json_power_data(json_t *get_power_obj)
     double value = 0.0;
     int d;
     unsigned int chipid;
-
+    rsmi_status_t ret;
+    uint32_t num_devices;
+    int gpus_per_socket;
     char hostname[1024];
+    
     struct timeval tv;
     uint64_t ts;
     unsigned int nsockets;
