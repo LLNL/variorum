d25ef56febb061d17ea6adab58d5363239433645
Tapasya Patki <patki1@llnl.gov>
2024-03-01 01:52:01 -0800

    Revert common.c to dev, will not build

--- src/powmon/common.c
+++ src/powmon/common.c
@@ -15,7 +15,6 @@ struct thread_args
 {
     bool measure_all;
     unsigned long sample_interval;
-    bool power_with_util;
 };
 
 int init_data(void)
@@ -23,304 +22,199 @@ int init_data(void)
     return 0;
 }
 
-void parse_power_and_util_json_obj(char *pow_str, char *util_str,
-                                   int num_sockets)
+void parse_json_power_obj(char *s, int num_sockets)
 {
-    int i, j;
-    char hostname[1024];
-    char socket_num[11];
-    size_t size;
-    int ndevices, ndevices_socket ;
-    gethostname(hostname, 1024);
+    const char *hostname = NULL;
+    char header_str[500] = {'\0'}; //A big allocation at the moment.
+    char value_str[1000] = {'\0'}; //A big allocation at the moment.
+    char temp_value_str[100] = {'\0'};
+    json_t *node_obj = NULL;
+    json_t *power_obj = json_loads(s, JSON_DECODE_ANY, NULL);
+    void *iter = json_object_iter(power_obj);
 
-    /* Create a Jansson based JSON structure */
-    json_t *util_obj = NULL;
-
-    double cpu_util, mem_util, sys_util, user_util;
-    int gpu_util;
     static bool write_header = true;
 
-    /* Create a Jansson based JSON structure */
-    json_t *power_obj = NULL;
-
-    double power_node, power_cpu, power_gpu, power_mem;
-    static char **json_metric_names = NULL;
-
-    /* List of socket-level JSON object metrics */
-    static const char *metrics[] = {"power_cpu_watts_socket_",
-                                    "power_gpu_watts_socket_",
-                                    "power_mem_watts_socket_"
-                                   };
-    /* Allocate space for metric names */
-    json_metric_names = malloc(3 * num_sockets * sizeof(char *));
-    for (i = 0; i < (3 * num_sockets); i++)
+    /* This is tailored to the nested structure that we have created (see docs). */
+    /* Just for the first level, we use the iterator to obtain the hostname,
+     * as this is encoded as a key to reduce verbosity. */
+    while (iter)
     {
-        json_metric_names[i] = malloc(40);
-    }
-
-    for (i = 0; i < num_sockets; i++)
-    {
-        /* Create a metric name list for querying json object later on */
-        for (j = 0; j < 3; j++)
+        hostname = json_object_iter_key(iter);
+        node_obj = json_object_iter_value(iter);
+        if (node_obj == NULL)
         {
-            char current_metric[40];
-            char current_socket[16];
-            strcpy(current_metric, metrics[j]);
-            sprintf(current_socket, "%d", i);
-            strcat(current_metric, current_socket);
-            strcpy(json_metric_names[(num_sockets * j) + i], current_metric);
+            printf("JSON object not found");
+            exit(0);
         }
+        /* The following should return NULL after the first call per our object. */
+        iter = json_object_iter_next(power_obj, iter);
     }
 
-    /* Load the string as a JSON object using Jansson */
-    power_obj = json_loads(pow_str, JSON_DECODE_ANY, NULL);
+    uint64_t timestamp;
+    int num_gpus_per_socket = -1;
 
-    /* Extract and print values from JSON object */
-    power_node = json_real_value(json_object_get(power_obj, "power_node_watts"));
-
-    //static char **json_metric_names = NULL;
-
-    /* List of socket-level JSON object metrics */
-    /*static const char *metrics[] = {"power_cpu_watts_socket_",
-                                    "power_gpu_watts_socket_",
-                                    "power_mem_watts_socket_"
-                                   };*/
-
-    /* Allocate space for metric names */
-    /*json_metric_names = malloc(3 * num_sockets * sizeof(char *));
-    for (i = 0; i < (3 * num_sockets); i++)
-    {
-        json_metric_names[i] = malloc(40);
-    }
-
-    for (i = 0; i < num_sockets; i++)
-    {
-        // Create a metric name list for querying json object later on /
-        for (j = 0; j < 3; j++)
-        {
-            char current_metric[40];
-            char current_socket[16];
-            strcpy(current_metric, metrics[j]);
-            sprintf(current_socket, "%d", i);
-            strcat(current_metric, current_socket);
-            strcpy(json_metric_names[(num_sockets * j) + i], current_metric);
-        }
-    }*/
-
-    /* Load the string as a JSON object using Jansson */
-    util_obj = json_loads(util_str, JSON_DECODE_ANY, NULL);
-
-
-
-    /* Extract and print values from JSON object */
-    json_t *host_obj = json_object_get(util_obj, hostname);
-    mem_util = json_real_value(json_object_get(host_obj, "memory_util%"));
-    json_t *cpu_util_obj = json_object_get(host_obj, "CPU");
-    cpu_util = json_real_value(json_object_get(cpu_util_obj, "total_util%"));
-    sys_util = json_real_value(json_object_get(cpu_util_obj, "system_util%"));
-    user_util = json_real_value(json_object_get(cpu_util_obj, "user_util%"));
-    json_t *gpu_obj = json_object_get(host_obj, "GPU");
-
+    //  Extract node-levels value from node object
+    timestamp = json_integer_value(json_object_get(node_obj, "timestamp"));
 
     if (write_header == true)
     {
-        fprintf(logfile, "%s,%s,", "Timestamp (ms)", "Node Power (W)");
-        for (i = 0; i < num_sockets; i++)
-        {
-            char str[40];
-            sprintf(str, "Socket %i CPU Power (W)", i);
-            fprintf(logfile, "%s,", str);
-            sprintf(str, "Socket %i GPU Power (W)", i);
-            fprintf(logfile, "%s,", str);
-            sprintf(str, "Socket %i Mem Power (W)", i);
-            fprintf(logfile, " %s,", str);
+        strcpy(header_str, "Hostname,Timestamp,");
+    }
+    sprintf(temp_value_str, "%s,%ld,", hostname, timestamp);
+    strcat(value_str, temp_value_str);
+    // printf("Hostname: %s\n", hostname);
+    // printf("Timestamp: %lu\n", timestamp);
 
+    // If we're on a GPU-only build, we don't have power_node_watts.
+    if (json_object_get(node_obj, "power_node_watts") != NULL)
+    {
+        double power_node;
+        power_node = json_real_value(json_object_get(node_obj, "power_node_watts"));
+        if (write_header == true)
+        {
+            strcat(header_str, "Node Power (W),");
+        }
+        sprintf(temp_value_str, "%0.2lf,", power_node);
+        strcat(value_str, temp_value_str);
+        //printf("Node Power: %0.2lf Watts\n", power_node);
+    }
+
+    // If we're on a CPU-only build, we don't have num_gpus_per_socket.
+    // Powmon doesn't need to print this, but needs to know this value.
+    if (json_object_get(node_obj, "num_gpus_per_socket") != NULL)
+    {
+        num_gpus_per_socket = json_integer_value(json_object_get(node_obj,
+                              "num_gpus_per_socket"));
+        //printf("Number of GPUs per socket: %d\n", num_gpus_per_socket);
+    }
+
+    //  Extract socket and GPU-level data from the node object
+
+    int i;
+    char socketID[20];
+
+    for (i = 0; i < num_sockets; ++i)
+    {
+        snprintf(socketID, 20, "socket_%d", i);
+        json_t *socket_obj = json_object_get(node_obj, socketID);
+        if (socket_obj == NULL)
+        {
+            printf("Socket object not found!\n");
+            exit(0);
         }
 
-        fprintf(logfile, "%s,%s,%s,%s,", "Memory Utiilizaion%",
-                "Total CPU Utilization%", "User Utilization%", "System Utilization%");
-        for (i = 0; i < num_sockets; ++i)
+        //If we're on a GPU-only build, we don't have power_cpu_watts
+        if (json_object_get(socket_obj, "power_cpu_watts") != NULL)
         {
-            sprintf(socket_num, "Socket_%d", i);
-            json_t *socket_obj = json_object_get(gpu_obj, socket_num);
-            size = json_object_size(socket_obj);
-            ndevices  = size;
-            for (j = 0; j < ndevices; j++)
+            double power_cpu;
+            power_cpu = json_real_value(json_object_get(socket_obj, "power_cpu_watts"));
+            if (write_header == true)
             {
-                char device_id[12];
-                snprintf(device_id, 12, "GPU%d%d_util%", i, j);
-                // Don't write out a comma after the last column name
-                if ((i + 1) == num_sockets && (j + 1) == ndevices)
+                sprintf(temp_value_str, "Socket_%d Power (W),", i);
+                strcat(header_str, temp_value_str);
+            }
+            sprintf(temp_value_str, "%0.2lf,", power_cpu);
+            strcat(value_str, temp_value_str);
+
+            //printf("Socket %d, CPU Power: %0.2lf Watts\n", i, power_cpu);
+        }
+
+        //If we're on a GPU-only build, we don't have power_mem_watts
+        if (json_object_get(socket_obj, "power_mem_watts") != NULL)
+        {
+            double power_mem;
+            power_mem = json_real_value(json_object_get(socket_obj, "power_mem_watts"));
+            if (write_header == true)
+            {
+                if (((i + 1) == num_sockets) && (num_gpus_per_socket < 0))
                 {
-                    fprintf(logfile, "%s\n", device_id);
-                    write_header = false;
+                    // Add a newline as we've reached the last column
+                    sprintf(temp_value_str, "Mem_%d Power (W)\n", i);
                 }
                 else
                 {
-                    fprintf(logfile, "%s,", device_id);
+                    sprintf(temp_value_str, "Mem_%d Power (W),", i);
                 }
+
+                strcat(header_str, temp_value_str);
             }
-        }
-    }
 
-    fprintf(logfile, "%ld, %lf, ", now_ms(), power_node);
-
-    for (i = 0; i < num_sockets; i++)
-    {
-        power_cpu = json_real_value(json_object_get(power_obj, json_metric_names[i]));
-        power_gpu = json_real_value(json_object_get(power_obj,
-                                    json_metric_names[num_sockets + i]));
-        power_mem = json_real_value(json_object_get(power_obj,
-                                    json_metric_names[(num_sockets * 2) + i]));
-        fprintf(logfile, "%lf, %lf, %lf,", power_cpu, power_gpu, power_mem);
-    }
-
-
-    fprintf(logfile, "%lf, %lf, %lf, %lf, ", mem_util, cpu_util, user_util,
-            sys_util);
-    for (i = 0; i < num_sockets; ++i)
-    {
-        sprintf(socket_num, "Socket_%d", i);
-        json_t *socket_obj = json_object_get(gpu_obj, socket_num);
-        size = json_object_size(socket_obj);
-        ndevices  = size;
-        for (j = 0; j < ndevices; j++)
-        {
-            char device_id[12];
-            snprintf(device_id, 12, "GPU%d%d_util%", i, j);
-            gpu_util = json_integer_value(json_object_get(socket_obj, device_id));
-            // Don't write out a comma after the last column name
-            if ((i + 1) == num_sockets && (j + 1) == ndevices)
+            if (((i + 1) == num_sockets) && (num_gpus_per_socket < 0))
             {
-                fprintf(logfile, "%d", gpu_util);
-                fprintf(logfile, "\n");
+                // Add a newline as we've reached the last column
+                sprintf(temp_value_str, "%0.2lf\n", power_mem);
             }
             else
             {
-                fprintf(logfile, "%d,", gpu_util);
+                sprintf(temp_value_str, "%0.2lf,", power_mem);
             }
+
+            strcat(value_str, temp_value_str);
+            //printf("Socket %d, Mem Power: %0.2lf Watts\n", i, power_mem);
         }
-    }
 
-
-
-
-
-    /* Deallocate metric array */
-    for (i = 0; i < num_sockets * 3; i++)
-    {
-        free(json_metric_names[i]);
-    }
-    free(json_metric_names);
-
-    /*Deallocate JSON object*/
-    json_decref(util_obj);
-    json_decref(power_obj);
-}
-
-void parse_json_obj(char *s, int num_sockets)
-{
-    int i, j;
-    static bool write_header = true;
-
-    /* Create a Jansson based JSON structure */
-    json_t *power_obj = NULL;
-
-    double power_node, power_cpu, power_gpu, power_mem;
-    static char **json_metric_names = NULL;
-
-    /* List of socket-level JSON object metrics */
-    static const char *metrics[] = {"power_cpu_watts_socket_",
-                                    "power_gpu_watts_socket_",
-                                    "power_mem_watts_socket_"
-                                   };
-
-    /* Allocate space for metric names */
-    json_metric_names = malloc(3 * num_sockets * sizeof(char *));
-    for (i = 0; i < (3 * num_sockets); i++)
-    {
-        json_metric_names[i] = malloc(40);
-    }
-
-    for (i = 0; i < num_sockets; i++)
-    {
-        /* Create a metric name list for querying json object later on */
-        for (j = 0; j < 3; j++)
+        if (num_gpus_per_socket > 0)
         {
-            char current_metric[40];
-            char current_socket[16];
-            strcpy(current_metric, metrics[j]);
-            sprintf(current_socket, "%d", i);
-            strcat(current_metric, current_socket);
-            strcpy(json_metric_names[(num_sockets * j) + i], current_metric);
-        }
-    }
+            json_t *gpu_obj = json_object_get(socket_obj, "power_gpu_watts");
 
-    /* Load the string as a JSON object using Jansson */
-    power_obj = json_loads(s, JSON_DECODE_ANY, NULL);
-
-    /* Extract and print values from JSON object */
-    power_node = json_real_value(json_object_get(power_obj, "power_node_watts"));
-
-    if (write_header == true)
-    {
-        for (i = 0; i < num_sockets; i++)
-        {
-            char str[40];
-            sprintf(str, "Socket %i CPU Power (W)", i);
-            fprintf(logfile, "%s,", str);
-            sprintf(str, "Socket %i GPU Power (W)", i);
-            fprintf(logfile, "%s,", str);
-            if ((i + 1) == num_sockets)
+            if (gpu_obj == NULL)
             {
-                sprintf(str, "Socket %i Mem Power (W)", i);
-                fprintf(logfile, "%s\n", str);
+                printf("GPU object not found! \n");
+                exit(0);
             }
-            else
+            const char *key;
+            json_t *gpu_value;
+            char last_gpu_str[32];
+
+            json_object_foreach(gpu_obj, key, gpu_value)
             {
-                sprintf(str, "Socket %i Mem Power (W)", i);
-                fprintf(logfile, "%s,", str);
+                //Determine last GPU, and check to add newline instead of comma.
+                sprintf(last_gpu_str, "GPU_%d", ((num_gpus_per_socket * num_sockets) - 1));
+                if (write_header == true)
+                {
+                    if (strcmp(last_gpu_str, key) == 0)
+                    {
+                        sprintf(temp_value_str, "%s Power (W)\n", key);
+                    }
+                    else
+                    {
+                        sprintf(temp_value_str, "%s Power (W),", key);
+                    }
+
+                    strcat(header_str, temp_value_str);
+
+                }
+                if (strcmp(last_gpu_str, key) == 0)
+                {
+                    sprintf(temp_value_str, "%0.2lf\n", json_real_value(gpu_value));
+                }
+                else
+                {
+                    sprintf(temp_value_str, "%0.2lf,", json_real_value(gpu_value));
+                }
+
+                strcat(value_str, temp_value_str);
+                //printf("Socket %d, %s Power: %0.2lf Watts\n", i, key, json_real_value(value));
             }
-
-            if ((i + 1) == num_sockets)
-                write_header = false;
         }
-
-    }
-    fprintf(logfile, "%ld, %lf, ", now_ms(), power_node);
-
-    for (i = 0; i < num_sockets; i++)
-    {
-        power_cpu = json_real_value(json_object_get(power_obj, json_metric_names[i]));
-        power_gpu = json_real_value(json_object_get(power_obj,
-                                    json_metric_names[num_sockets + i]));
-        power_mem = json_real_value(json_object_get(power_obj,
-                                    json_metric_names[(num_sockets * 2) + i]));
 
         if ((i + 1) == num_sockets)
         {
-            fprintf(logfile, "%lf, %lf, %lf", power_cpu, power_gpu, power_mem);
-            fprintf(logfile, "\n");
+            // Write the header and set flag to false
+            fprintf(logfile, "%s", header_str);
+            write_header = false;
         }
-        else
-        {
-            fprintf(logfile, "%lf, %lf, %lf,", power_cpu, power_gpu, power_mem);
-        }
+
     }
 
-    /* Deallocate metric array */
-    for (i = 0; i < num_sockets * 3; i++)
-    {
-        free(json_metric_names[i]);
-    }
-    free(json_metric_names);
+    //Write the values now
+    fprintf(logfile, "%s", value_str);
 
-    /*Deallocate JSON object*/
+    // Clean up memory.
     json_decref(power_obj);
 }
 
-
-void take_measurement(bool measure_all, bool power_with_util)
+void take_measurement(bool measure_all)
 {
 #if 0
     uint64_t instr0 = 0;
@@ -335,10 +229,9 @@ void take_measurement(bool measure_all, bool power_with_util)
     if (measure_all == false)
     {
         // Extract power information from Variorum JSON API
-        int ret_util, ret_pow;
+        int ret;
         int num_sockets = 0;
-        char *power_str = NULL;
-        char *util_str = NULL;
+        char *s = NULL;
 
         num_sockets = variorum_get_num_sockets();
 
@@ -348,45 +241,23 @@ void take_measurement(bool measure_all, bool power_with_util)
             exit(-1);
         }
 
-        //ret = variorum_get_node_power_json(&s);
-        if (power_with_util == true)
+        ret = variorum_get_power_json(&s);
+        if (ret != 0)
         {
-            ret_util = variorum_get_node_utilization_json(&util_str);
-            if (ret_util != 0)
-            {
-                printf("JSON get node utilization failed. Exiting.\n");
-                free(util_str);
-                exit(-1);
-            }
-
-
-            ret_pow = variorum_get_node_power_json(&power_str);
-            if (ret_pow != 0)
-            {
-                printf("JSON get node power failed. Exiting.\n");
-                free(power_str);
-                exit(-1);
-            }
-
-            parse_power_and_util_json_obj(power_str, util_str, num_sockets);
+            printf("JSON get node power failed. Exiting.\n");
+            free(s);
+            exit(-1);
         }
-        else
-        {
-            ret_pow = variorum_get_node_power_json(&power_str);
-            if (ret_pow != 0)
-            {
-                printf("JSON get node power failed. Exiting.\n");
-                free(power_str);
-                exit(-1);
-            }
-            // Write out to logfile
-            parse_json_obj(power_str, num_sockets);
-        }
+
+        // Write out to logfile
+        parse_json_power_obj(s, num_sockets);
     }
 
     // Verbose output with all sensors/registers
     if (measure_all == true)
+    {
         variorum_monitoring(logfile);
+    }
 
 #if 0
     total_joules += rapl_data[0] + rapl_data[1];
@@ -420,20 +291,18 @@ void *power_measurement(void *arg)
     struct thread_args th_args;
     th_args.sample_interval = (*(struct thread_args *)arg).sample_interval;
     th_args.measure_all = (*(struct thread_args *)arg).measure_all;
-    th_args.power_with_util = (*(struct thread_args *)arg).power_with_util;
     // According to the Intel docs, the counter wraps at most once per second.
     // 50 ms should be short enough to always get good information (this is
     // default).
     printf("Using sampling interval of: %ld ms\n", th_args.sample_interval);
     printf("Using verbosity of: %d\n", th_args.measure_all);
-    printf("Using verbosity of: %d\n", th_args.power_with_util);
     init_msTimer(&timer, th_args.sample_interval);
     start = now_ms();
 
     timer_sleep(&timer);
     while (running)
     {
-        take_measurement(th_args.measure_all, th_args.power_with_util);
+        take_measurement(th_args.measure_all);
         timer_sleep(&timer);
     }
     return arg;
