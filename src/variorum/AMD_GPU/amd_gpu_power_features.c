8469aa0da60314cdda2700695190b0616f6b648c
Tapasya Patki <patki1@llnl.gov>
2024-06-14 18:46:32 -0700

    Update amd_gpu_power_features.c comment

--- src/variorum/AMD_GPU/amd_gpu_power_features.c
+++ src/variorum/AMD_GPU/amd_gpu_power_features.c
@@ -78,8 +78,8 @@ void get_power_data(int chipid, int total_sockets, int verbose, FILE *output)
         /* Variorum v0.8 will support the new API from ROCm 6.0.2, which
         * adds the RSMI_POWER_TYPE enum and the rsmi_dev_power_get() API.
         * If using an older version of ROCm, please use the code segment
-        * with the rsmi_dev_power_ave_get() API below on lines 85 instead of
-        * line 89. We're not adding backward compatibility checks
+        * with the rsmi_dev_power_ave_get() API below on lines 85 and comment
+        * lines 88 and 89. We're not adding backward compatibility checks
         * at the moment due to lack of resources and time.
         *
         *  ret = rsmi_dev_power_ave_get(i, 0, &pwr_val);
