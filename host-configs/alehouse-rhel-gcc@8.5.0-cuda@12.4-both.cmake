eaa04152a60fc62710a702bb6d752d93313ca333
Stephanie Brink <brink2@llnl.gov>
2025-04-04 14:32:21 -0700

    Fix alehouse CPU-only build (#577)
    * add missing pthread library
    
    * move cmake minimum version declaration earlier to fix warning
    
    * update paths for cuda 12.4

--- host-configs/alehouse-rhel-gcc@8.5.0-cuda@12.4-both.cmake
+++ host-configs/alehouse-rhel-gcc@8.5.0-cuda@12.4-both.cmake
@@ -32,4 +32,4 @@ set(HWLOC_DIR "/usr/local/share/hwloc-1.11.7-install" CACHE PATH "")
 # path to global jansson install
 set(JANSSON_DIR "/usr/local/share/jansson-2.6-install" CACHE PATH "")
 
-set(NVML_DIR "/usr/local/cuda-12.0" CACHE PATH "")
+set(NVML_DIR "/usr/local/cuda-12.4/targets/ppc64le-linux" CACHE PATH "")
